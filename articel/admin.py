b2d2fbedc6099cdaac10d43a948f6731446e97f7
Overlord-zerg-more-minerals <fool_fook_hook12345@mail.ru>
2020-06-06 18:19:27 +0600

    fix things

--- articel/admin.py
+++ articel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from article.models import *
+from articel.models import *
 
 admin.site.register(Article)
 admin.site.register(Author)
--- articel/models.py
+++ articel/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrip.auth.models import User
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
@@ -8,7 +8,7 @@ class Article(models.Model):
     likes = models.IntegerField(default=0)
     activate = models.BooleanField(default=True)
     author = models.ForeignKey(
-        to='Author', on_delete=models.CASCADE,
+        to="Author", on_delete=models.CASCADE,
         related_name="articles", null=True, blank=True
     )
     readers = models.ManyToManyField(to=User, related_name="articles")
