b6a9610405fa15606b6afc123e23d625e87397d4
Overlord-zerg-more-minerals <fool_fook_hook12345@mail.ru>
2020-07-02 21:39:07 +0600

    fixed settings, static-root

--- itblog/urls.py
+++ itblog/urls.py
@@ -32,3 +32,4 @@ urlpatterns = [
     path("comments/<int:id>/edit/", edit_comment, name="edit-comment"),
     path("comment/<int:id>/delete/", delete_comment, name="delete-comment")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
